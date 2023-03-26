f02f372a9dbc67bf626114d06fa2ca2d0e92cc00
odanzhou <odanzhou@gmail.com>
2023-03-26 15:27:45 +0800

    feat: 增加入口文件和输出

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,10 +1,16 @@
 const Webpack = require('webpack')
+const path = require('path')
 
 /**
  * @type { Webpack.Configuration }
  */
 module.exports = {
   mode: 'null',
+  entry: './src/pages/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
   module: {
     rules: [
       {
